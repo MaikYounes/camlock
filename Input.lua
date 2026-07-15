96c54fb1e321ca633ac9d89acae7307c3ad9b14c
Valkyria <137658808+MaikYounes@users.noreply.github.com>
2026-07-15 16:32:21 -0300

    Update Input.lua

--- Input.lua
+++ Input.lua
@@ -1,1 +1,63 @@
+local Input = {}
 
+local UserInputService = game:GetService("UserInputService")
+
+local Maid
+local Config
+
+local inputMaid
+
+
+function Input:Init(maidModule, configModule)
+
+	Maid = maidModule
+	Config = configModule
+
+	inputMaid = Maid.new()
+
+end
+
+
+function Input:Bind(key, callback)
+
+	inputMaid:Give(
+		UserInputService.InputBegan:Connect(function(input, processed)
+
+			if processed then
+				return
+			end
+
+			if input.KeyCode == key then
+				callback()
+			end
+
+		end)
+	)
+
+end
+
+
+function Input:Start(actions)
+
+	for key,callback in pairs(actions) do
+
+		self:Bind(
+			key,
+			callback
+		)
+
+	end
+
+end
+
+
+function Input:Destroy()
+
+	if inputMaid then
+		inputMaid:Cleanup()
+	end
+
+end
+
+
+return Input
